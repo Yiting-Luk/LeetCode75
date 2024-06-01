390aed695af922af754df92b87bab7f82f18e394
Yiting Lu <luyiting@luyitingdembp.kpn>
2024-06-01 22:55:30 +0200

    .

---
+++ binaryTree/maxDepth.py
@@ -0,0 +1,53 @@
+import math
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+class BinaryTree:
+    def __init__(self, val):
+        self.root = TreeNode(val) 
+    def insert(self, val):
+        node = TreeNode(val)
+        if self.root == None:
+            self.root = node
+            return
+        queue = [self.root]
+        while queue:
+            curr_node = queue.pop(0)
+            if curr_node.left == None:
+                curr_node.left = node
+                break
+            else:
+                if curr_node.left.val != None:
+                    queue.append(curr_node.left)                
+            if curr_node.right == None:
+                curr_node.right = node
+                break
+            else:
+                if curr_node.right.val != None:
+                    queue.append(curr_node.right)
+
+class Solution(object):
+    def maxDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if not root:
+            return 0
+        else:
+            left_depth = self.maxDepth(root.left)
+            right_depth = self.maxDepth(root.right)
+            return max(left_depth, right_depth) + 1
+        
+root = [3,9,20,None,None,15,7]
+root = [1,2,None,3,None,4,None,5]
+tree = BinaryTree(root[0])
+for i in range(1, len(root)):
+    # print(root[i])
+    tree.insert(root[i])
+obj = Solution()
+result = obj.maxDepth(tree.root)
+print(result)
